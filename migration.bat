e3a35599c382914f6c4b65bc7d9d1c6620d13f4c
Bekalar <k175bed@gmail.com>
2024-05-22 18:21:59 +0200

    Ebook entity

---
+++ migration.bat
@@ -0,0 +1,12 @@
+@echo off
+
+if [%2]==[-db] goto rebuildDB
+goto :EOF
+
+:rebuildDB
+echo "rebuilding database..."
+php bin/console doctrine:schema:drop -n -q --force --full-database
+del /f .\migrations\*.php
+php bin/console make:migration
+php bin/console doctrine:migrations:migrate -n -q
+@REM php bin/console doctrine:fixtures:load -n -q
---
+++ src/Repository/EbookRepository.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Ebook;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Ebook>
+ */
+class EbookRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Ebook::class);
+    }
+
+//    /**
+//     * @return Ebook[] Returns an array of Ebook objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('e.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Ebook
+//    {
+//        return $this->createQueryBuilder('e')
+//            ->andWhere('e.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
